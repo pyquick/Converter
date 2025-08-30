57b61528ca7e3a8544b5d66c56b044b52528c0fa
Code Jumping <fairzlq@hotmail.com>
2025-08-30 18:28:20 +0800

    add intel support

--- buildzip.py
+++ buildzip.py
@@ -1,4 +1,7 @@
 from support.convertzip import create_zip
 def build_zip():
     print("Building Zip")
-    create_zip("./dist/Converter.zip",["./dist/Converter.app"])
+    create_zip("./dist/Converter_arm64_darwin.zip",["./dist/Converter.app"])
+def build_zip_intel():
+    print("Building Zip")
+    create_zip("./dist/Converter_intel.zip",["./dist/Converter.app"])
